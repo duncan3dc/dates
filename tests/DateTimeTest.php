e31e2f5532430e3436679a01fb81130edf7e872c
Craig Duncan <duncanc@regatta.com>
2015-01-05 20:42:50 +0000

    Created some methods to format dates

--- tests/DateTimeTest.php
+++ tests/DateTimeTest.php
@@ -29,4 +29,30 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException("InvalidArgumentException", "An invalid unix timestamp was passed");
         $date = new DateTime("ABC");
     }
+
+
+    public function testFormat1()
+    {
+        $unix = time();
+        $date = new DateTime($unix);
+        $this->assertSame(date("d/m/y", $unix), $date->format("d/m/y"));
+    }
+    public function testFormat2()
+    {
+        $unix = time();
+        $date = new DateTime($unix);
+        $this->assertSame((int) date("Ymd", $unix), $date->format("Ymd"));
+    }
+    public function testNumeric()
+    {
+        $unix = time();
+        $date = new DateTime($unix);
+        $this->assertSame((int) date("Ymd", $unix), $date->numeric("Ymd"));
+    }
+    public function testString()
+    {
+        $unix = time();
+        $date = new DateTime($unix);
+        $this->assertSame(date("Ymd", $unix), $date->string("Ymd"));
+    }
 }
