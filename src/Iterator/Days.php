6d5538e538d20cab74a4927cced5d59865a4570f
Craig Duncan <duncanc@regatta.com>
2015-01-07 08:55:48 +0000

    Abstract out the bulk of the Days Iterator
    This allows the iterators themselves to be very lightweight, and others can be created easily

--- src/Iterator/Days.php
+++ src/Iterator/Days.php
@@ -7,29 +7,8 @@ use Regatta\Dates\DateTime;
 /**
  * An iterator for the number of days in a range.
  */
-class Days implements \Iterator
+class Days extends AbstractIterator
 {
-    /**
-     * @var int $start The unix timestamp for the start date of the range
-     */
-    protected $start;
-
-    /**
-     * @var int $end The unix timestamp for the end date of the range
-     */
-    protected $end;
-
-    /**
-     * @var int $position The current position in the iterator
-     */
-    protected $position;
-
-    /**
-     * @var DateTime $date The date object for the current iterator position
-     */
-    protected $date;
-
-
     /**
      * Create a new iterator for the number of days in a range.
      *
@@ -45,58 +24,12 @@ class Days implements \Iterator
 
 
     /**
-     * Get the current iterator value
-     *
-     * @return DateTime
-     */
-    public function current()
-    {
-        return $this->date;
-    }
-
-
-    /**
-     * Get the current iterator position
-     *
-     * @return int
-     */
-    public function key()
-    {
-        return $this->position;
-    }
-
-
-    /**
-     * Advance the iterator to the next position
+     * Increment the internal date to the next position in the range.
      *
      * @return void
      */
-    public function next()
+    protected function increment()
     {
-        ++$this->position;
         $this->date = new DateTime(mktime(12, 0, 0, $this->date->numeric("n"), $this->date->numeric("j") + 1, $this->date->numeric("Y")));
-    }
-
-
-    /**
-     * Reset the iterator to the start
-     *
-     * @return void
-     */
-    public function rewind()
-    {
-        $this->position = 0;
-        $this->date = new DateTime($this->start);
-    }
-
-
-    /**
-     * Check if the current position is valid
-     *
-     * @return boolean
-     */
-    public function valid()
-    {
-        return $this->date->midday() <= $this->end;
     }
 }
