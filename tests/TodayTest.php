bb7fd2dc64437c219d79a1e94bb45b146351cc98
Craig Duncan <duncanc@regatta.com>
2015-01-15 20:58:00 +0000

    Ensure the date class always uses midday

--- tests/TodayTest.php
+++ tests/TodayTest.php
@@ -7,8 +7,7 @@ class TodayTest extends \PHPUnit_Framework_TestCase
 
     public function testConstructor()
     {
-        $unix = time();
         $date = new Today;
-        $this->assertSame($unix, $date->asUnix());
+        $this->assertSame(mktime(12, 0, 0, date("n"), date("j"), date("Y")), $date->asUnix());
     }
 }
