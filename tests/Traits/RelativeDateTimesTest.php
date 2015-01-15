9ee514d7a5fcbc642a4185d09843fdf9e9f8dcf0
Craig Duncan <duncanc@regatta.com>
2015-01-15 22:42:09 +0000

    Create relative time methods for adding hours/minutes/seconds

--- tests/Traits/RelativeDateTimesTest.php
+++ tests/Traits/RelativeDateTimesTest.php
@@ -61,4 +61,52 @@ class RelativeDateTimesTest extends \PHPUnit_Framework_TestCase
         $result = $date->prevYear();
         $this->assertSame(mktime(0, 0, 0, 1, 1, 2000), $result->timestamp());
     }
+
+
+    public function testAddSeconds()
+    {
+        $date = new DateTime(mktime(12, 0, 0, 3, 31, 2014));
+        $result = $date->addSeconds(5);
+        $this->assertSame(mktime(12, 0, 5, 3, 31, 2014), $result->timestamp());
+    }
+
+
+    public function testSubSeconds()
+    {
+        $date = new DateTime(mktime(12, 0, 0, 3, 31, 2014));
+        $result = $date->subSeconds(60);
+        $this->assertSame(mktime(11, 59, 0, 3, 31, 2014), $result->timestamp());
+    }
+
+
+    public function testAddMinutes()
+    {
+        $date = new DateTime(mktime(23, 59, 59, 3, 31, 1970));
+        $result = $date->addMinutes(5);
+        $this->assertSame(mktime(0, 4, 59, 4, 1, 1970), $result->timestamp());
+    }
+
+
+    public function testSubMinutes()
+    {
+        $date = new DateTime(mktime(12, 10, 0, 5, 1, 2012));
+        $result = $date->subMinutes(2);
+        $this->assertSame(mktime(12, 8, 0, 5, 1, 2012), $result->timestamp());
+    }
+
+
+    public function testAddHours()
+    {
+        $date = new DateTime(mktime(23, 59, 59, 12, 31, 1986));
+        $result = $date->addHours(1);
+        $this->assertSame(mktime(0, 59, 59, 1, 1, 1987), $result->timestamp());
+    }
+
+
+    public function testSubHours()
+    {
+        $date = new DateTime(mktime(0, 0, 0, 2, 29, 2012));
+        $result = $date->subHours(24);
+        $this->assertSame(mktime(0, 0, 0, 2, 28, 2012), $result->timestamp());
+    }
 }
