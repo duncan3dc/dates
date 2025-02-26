33cc07b24e282a63a9a331beb5f69563c3192a0d
Craig Duncan <craig.duncan@estrato.co.uk>
2025-02-26 15:29:36 +0000

    Ensure any warnings/deprecations are converted to exceptions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . '/../vendor/autoload.php';
+
+# Convert any issues to exceptions
+set_error_handler(static function ($errno, $errstr, $errfile, $errline): bool {
+    if (!(error_reporting() & $errno)) {
+        return false;
+    }
+    throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+}, E_ALL);
